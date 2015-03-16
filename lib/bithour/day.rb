ad48616377689b6cd61b0b6500a6c36e9527b870
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-03-16 23:51:18 +0900

    Extract a method

--- lib/bithour/day.rb
+++ lib/bithour/day.rb
@@ -5,16 +5,21 @@ module Bithour
     end
 
     def add(first_hour, last_hour)
-      hours = "%024d" % @day.to_s(2)
-      hours.reverse!
-      first_hour.upto(last_hour) do |i|
-        hours[i] = "1"
-      end
-      @day = hours.reverse.to_i(2)
+      update(first_hour, last_hour, "1")
     end
 
     def include?(hour)
       @day[hour] == 1
+    end
+
+    private
+    def update(first_hour, last_hour, bit)
+      hours = "%024d" % @day.to_s(2)
+      hours.reverse!
+      first_hour.upto(last_hour) do |i|
+        hours[i] = bit
+      end
+      @day = hours.reverse.to_i(2)
     end
   end
 end
